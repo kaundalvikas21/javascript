7a4dd671f966c703cdb08f6b41276d7619b01f70
vikas kaundal <vikaskaundal21@gmail.com>
2023-10-23 18:06:22 +0530

    commit

--- 2btn.js
+++ 2btn.js
@@ -1,14 +1,23 @@
 var btn1 =document.querySelector('.red');
-var btn2 =document.querySelector('.green');
+// var btn2 =document.querySelector('.green');
+// var reset = document.querySelector(".Reset");
+
 
 btn1.addEventListener('click', function(){
-    btn1.style.backgroundColor = 'red';
-    btn2.style.backgroundColor = ''; //reset backgroundColor of btn-2
-    console.log('red');
-});
-btn2.addEventListener('click', function(){
-    btn2.style.backgroundColor = 'green';
-    btn1.style.backgroundColor = ''; //reset backgroundColor of btn-1
-    console.log('green');
     
+    var a = Math.floor(Math.random() * 1000) + 1;
+    var b = Math.floor(Math.random() * 1000) + 1;
+    var c = Math.floor(Math.random() * 1000) + 1;
+    var prefix = 'rgb';
+    let color = prefix + "("+a + "," + b +"," + c+")";
+    console.log(color)
+    document.body.style.backgroundColor = color;
 });
+// btn2.addEventListener('click', function(){
+//     document.body.style.backgroundColor = 'green';
+//     console.log('green');
+// });
+// reset.addEventListener('click', function(){
+//     document.body.style.backgroundColor = '';
+//     console.log('green');
+// });
--- add.js
+++ add.js
@@ -1,13 +1,61 @@
-var button = document.querySelector('button');
-let input1 = document.querySelector('.num1');
-let input2 = document.querySelector('.num2');
-let showVal = document.querySelector('.showVal');
+// var button = document.querySelector("button");
+// let input1 = document.querySelector(".num1");
+// let input2 = document.querySelector(".num2");
+// let showVal = document.querySelector(".showVal");
+// var color = document.querySelector("body");
 
-button.addEventListener('click', function(e){
-    e.preventDefault();
-    let sum = parseInt(input1.value) + parseInt(input2.value);
-    // console.log(sum);
-    // console.log(typeof(sum));
-    showVal.textContent = sum;
-    // return sum;
-})
+// button.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   // console.log(sum);
+//   // console.log(typeof(sum));
+//   // return sum;
+
+//   let sum = parseFloat(input1.value) + parseFloat(input2.value);
+//   if (sum > 50 && sum <= 100) {
+//     showVal.textContent = Math.round(sum);
+//     document.body.style.backgroundColor = "";
+//   } else if (sum == 50) {
+//     document.body.style.backgroundColor = "red";
+//     showVal.textContent = Math.round(sum);
+//   } else {
+//     showVal.textContent = "HELLO";
+//     document.body.style.backgroundColor = "";
+//   };
+// });
+
+
+let input1 = document.querySelector(".num1");
+let input2 = document.querySelector(".num2");
+let showVal = document.querySelector(".showVal");
+let calculateButton = document.getElementById("calculateButton");
+let form = document.querySelector("form");
+
+calculateButton.addEventListener("click", function () {
+  calculateSum();
+});
+
+form.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    calculateSum();
+  }
+});
+
+function calculateSum() {
+  let num1 = parseFloat(input1.value);
+  let num2 = parseFloat(input2.value);
+  let sum = num1 + num2;
+
+  switch (true) {
+    case sum === 50:
+      document.body.style.backgroundColor = "red";
+      showVal.textContent = Math.round(sum);
+      break;
+    case sum > 50 && sum <= 100:
+      showVal.textContent = Math.round(sum);
+      document.body.style.backgroundColor = "";
+      break;
+    default:
+      showVal.textContent = "The value is not in the desired range";
+      document.body.style.backgroundColor = "";
+  }
+}
