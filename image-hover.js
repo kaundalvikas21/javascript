f6c05a70343fe2462919dabb448f4c7b1cfaaa48
vikas kaundal <vikaskaundal21@gmail.com>
2023-10-25 21:46:27 +0530

    first commit

---
+++ image-hover.js
@@ -0,0 +1,35 @@
+var elem = document.querySelectorAll('.elem');
+
+elem.forEach(function (val) {
+
+    
+    val.addEventListener('mouseenter', function () {
+        val.childNodes[2].style.opacity = 1;
+       
+    });
+
+
+    val.addEventListener('mouseleave', function () {
+        val.childNodes[2].style.opacity = 0;
+    });
+   
+    val.addEventListener('mousemove', function (dets) {
+        val.childNodes[2].style.left = dets.x + "px";
+        val.childNodes[2].style.top = dets.y + "px";
+    });
+   
+});
+
+
+// var elemImage = document.querySelector('#elem1 img');
+
+// elem1.addEventListener('mousemove' ,function(dets){
+//     elemImage.style.left= dets.x+'px';
+//     elemImage.style.top= dets.y+'px';
+// })
+//     elem1.addEventListener('mouseenter' ,function(){
+//         elemImage.style.opacity = 1;
+// })
+//     elem1.addEventListener('mouseleave' ,function(){
+//         elemImage.style.opacity = 0;
+// })
